31f9e93e8aff931992847f03b4236c092a933526
Alex9220 <turtle1@mail.ru>
2021-10-24 22:46:23 +0300

    2

---
+++ src/main/java/Lesson1/Dog.java
@@ -0,0 +1,9 @@
+package Lesson1;
+
+public class Dog extends Animal {
+    public Dog(String name) {
+        super("Собака", name, 200, 20, 2);
+    }
+
+}
+
---
+++ src/main/java/Lesson1/Participant.java
@@ -0,0 +1,14 @@
+package Lesson1;
+
+public interface Participant {
+    void run(int runDistance);
+
+    void swim(int swimDistance);
+
+    void jump(int jumpHeight);
+
+    boolean isOnTrack();
+
+    void info();
+
+}
